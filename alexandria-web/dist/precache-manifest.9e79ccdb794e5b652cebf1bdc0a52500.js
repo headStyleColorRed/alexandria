fbf3acce827e62618265f4a91f91de2e7c7695cc
Rodrigo Labrador <headstylecolorred@gmail.com>
2021-01-04 16:48:21 +0100

    unignored dist

---
+++ alexandria-web/dist/precache-manifest.9e79ccdb794e5b652cebf1bdc0a52500.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b87dd9fa56ca55a564f",
+    "url": "/css/app.9e300104.css"
+  },
+  {
+    "revision": "340e317c4628d14a5c7a",
+    "url": "/css/chunk-vendors.a37e93ce.css"
+  },
+  {
+    "revision": "2a61b00fcc9b1afbf99be145777b1b72",
+    "url": "/fonts/MaterialIcons-Regular.2a61b00f.eot"
+  },
+  {
+    "revision": "51cf1d641ae503ece727e7d84b182524",
+    "url": "/fonts/MaterialIcons-Regular.51cf1d64.ttf"
+  },
+  {
+    "revision": "703cf8f274fbb265d49c6262825780e1",
+    "url": "/fonts/MaterialIcons-Regular.703cf8f2.woff2"
+  },
+  {
+    "revision": "84a37de85c17f186652a179b1145392f",
+    "url": "/fonts/MaterialIcons-Regular.84a37de8.woff"
+  },
+  {
+    "revision": "acf15f02ab541bc4c14481f58307e9a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b87dd9fa56ca55a564f",
+    "url": "/js/app.ebf288e9.js"
+  },
+  {
+    "revision": "340e317c4628d14a5c7a",
+    "url": "/js/chunk-vendors.02977b7f.js"
+  },
+  {
+    "revision": "01b747e4d22c320a2a30a8324940c4bb",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
